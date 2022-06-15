0e25deae03bb82916f5e4cfd3596fb2218c4b371
wanri parasian simanullang <wanriparasians@gmail.com>
2022-06-15 13:39:56 +0700

    selesai

--- index.js
+++ index.js
@@ -1,11 +1,57 @@
 // TODO: Buatlah data dari kedua user di sini
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
-const firstUser = {};
-const secondUser = {};
+const firstUser = {
+    name:'Monica',
+    gender:'Female',
+    age:17,
+    email:'monica@dingdong.com',
+    favoriteColor:['yellow', 'pink', 'white', 'purple'],
+    isHavePet:'Yes',
+    education:[
+        {name:'SD 01',
+        city:'Jakarta',
+        graduate:'2016',
+        },
+        {name:'SMP 02',
+        city:'Jakarta',
+        graduate:'2019',}
+        ,
+        {name:'SMA 03',
+        city:'Tanggerang',
+        graduate:''}],
+    favoriteRestaurant:['Bento', 'Sushi', 'Pancake', 'Eggy', 'Tempura', 'Bento', 'Eggy', 'Padang', 'Tteok', 'Sushi', 'Sushi']
+};
+const secondUser = {
+    name:'Wendy',
+    gender:'Male',
+    age:23,
+    email:'wendy@dingdong.com',
+    favoriteColor:['blue', 'black', 'gray'],
+    isHavePet:'No',
+    education:[
+        {name:'SD 02',
+        city:'Jakarta',
+        graduate:'2010',
+        },
+        {name:'SMP 03',
+        city:'Bogor',
+        graduate:'2013',}
+        ,
+        {name:'SMA 03',
+        city:'Tanggerang',
+        graduate:''},
+
+        {name:'Universitas Maju',
+        city:'Tanggerang',
+        graduate:''}],
+        
+
+    favoriteRestaurant:['Bento', 'Sushi', 'Pancake', 'Eggy', 'Tempura', 'Bento', 'Eggy', 'Padang', 'Tteok', 'Sushi', 'Sushi']
+};
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
-const users = [];
+const users = [firstUser,secondUser];
 
 // ! JANGAN MODIFIKASI LINE DI BAWAH
 function main() {
